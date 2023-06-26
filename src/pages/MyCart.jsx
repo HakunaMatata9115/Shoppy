bf4186bc84a75234e147fbc302ce35d46e766f80
Lucia <hakuna.matata.luna15@gmail.com>
2023-06-26 20:58:08 +1000

    💄 Style: Add cart ui css

--- src/pages/MyCart.jsx
+++ src/pages/MyCart.jsx
@@ -6,6 +6,7 @@ import CartItem from "../components/CartItem";
 import PriceCard from "../components/PriceCard";
 import { BsFillPlusCircleFill } from "react-icons/bs";
 import { FaEquals } from "react-icons/fa";
+import Button from "../components/ui/Button";
 
 export default function MyCart() {
   const SHIPPING = 3000;
@@ -23,24 +24,28 @@ export default function MyCart() {
     );
 
   return (
-    <section>
-      <p>내 장바구니</p>
+    <section className="p-8 flex flex-col">
+      <p className="text-2xl text-center font-bold pb-4 border-b border-gray-300">
+        내 장바구니
+      </p>
       {!hasProducts && <p>장바구니에 상품이 없습니다. 열심히 쇼핑해 주세요!</p>}
-      {hasProducts &&
-        products.map((product) => (
-          <>
-            <ul>
+      {hasProducts && (
+        <>
+          <ul className="border-b border-gray-300 mb-8 p-4 px-8">
+            {products.map((product) => (
               <CartItem key={product.id} product={product} uid={uid} />
-            </ul>
-            <div>
-              <PriceCard text="상품 총액" price={totalPrice} />
-              <BsFillPlusCircleFill />
-              <PriceCard text="배송비" price={SHIPPING} />
-              <FaEquals />
-              <PriceCard text="총 가격" price={totalPrice + SHIPPING} />
-            </div>
-          </>
-        ))}
+            ))}
+          </ul>
+          <div className="flex justify-between items-center px-2 md:px-8 lg:px-16 mb-6">
+            <PriceCard text="상품 총액" price={totalPrice} />
+            <BsFillPlusCircleFill className="shrink-0" />
+            <PriceCard text="배송비" price={SHIPPING} />
+            <FaEquals className="shrink-0" />
+            <PriceCard text="총 가격" price={totalPrice + SHIPPING} />
+          </div>
+          <Button text="주문하기" />
+        </>
+      )}
     </section>
   );
 }
