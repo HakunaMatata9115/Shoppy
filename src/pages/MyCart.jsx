53139d308b8f7118569b36d64bae813fb5c7eaed
Lucia <hakuna.matata.luna15@gmail.com>
2023-07-04 12:48:24 +1000

    ♻️ Refactor: Create custom hook for cart

--- src/pages/MyCart.jsx
+++ src/pages/MyCart.jsx
@@ -1,18 +1,16 @@
 import React from "react";
-import { useQuery } from "@tanstack/react-query";
-import { getCart } from "../api/firebase";
-import { useAuthContext } from "../context/AuthContext";
 import CartItem from "../components/CartItem";
 import PriceCard from "../components/PriceCard";
 import { BsFillPlusCircleFill } from "react-icons/bs";
 import { FaEquals } from "react-icons/fa";
 import Button from "../components/ui/Button";
+import useCart from "../hooks/useCart";
 
 export default function MyCart() {
   const SHIPPING = 3000;
-  const { uid } = useAuthContext();
-  const { isLoading, data: products } = useQuery(["carts"], () => getCart(uid));
-
+  const {
+    cartQuery: { isLoading, data: products },
+  } = useCart();
   if (isLoading) return <p>Loading...</p>;
 
   const hasProducts = products && products.length > 0;
@@ -33,7 +31,7 @@ export default function MyCart() {
         <>
           <ul className="border-b border-gray-300 mb-8 p-4 px-8">
             {products.map((product) => (
-              <CartItem key={product.id} product={product} uid={uid} />
+              <CartItem key={product.id} product={product} />
             ))}
           </ul>
           <div className="flex justify-between items-center px-2 md:px-8 lg:px-16 mb-6">
